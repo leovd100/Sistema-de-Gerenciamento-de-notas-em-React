9f138a80f762ae71edecf6e229acdf19153d88b2
Leonardo <demetrio.vd@hotmail.com>
2021-07-04 10:10:43 -0300

    Implementando sistema para apagar cards

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -13,7 +13,10 @@ class ListaDeNotas extends Component {
           return (
             <li key={index} className="lista-notas_item">
               
-              <CardNota titulo={nota.titulo} texto={nota.texto}/>
+              <CardNota titulo={nota.titulo} 
+              indice={index}
+              apagarNota={this.props.apagarNota} 
+              texto={nota.texto}/>
             </li>
           );
         })}
